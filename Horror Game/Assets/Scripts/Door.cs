581fc5e6c61e0daeaf6cc599894d8105a5fed2ea
Reyth <gethan101@gmail.com>
2025-05-02 22:32:11 +1200

    added audio to the game/some fx for the doorn some bg ambience, also touched up the script that shows when interaction with the door to display text saying the doors locked

--- Horror Game/Assets/Scripts/Door.cs
+++ Horror Game/Assets/Scripts/Door.cs
@@ -1,16 +1,63 @@
 using UnityEngine;
+using System.Collections;
 
 public class Door : MonoBehaviour
 {
+    [SerializeField] bool canOpen;
+    [SerializeField] GameObject thePlayer;
+    [SerializeField] GameObject theCam;
+    [SerializeField] GameObject commandBox;
+    [SerializeField] GameObject interactCross;
+    [SerializeField] GameObject Cross;
+    [SerializeField] GameObject textDesc;
+    [SerializeField] AudioSource lockedDoor;
+ 
+    void Update()
+    {
+        if (canOpen == true)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                StartCoroutine(OpeningDoor());
+            }
+        }
+    }
+
     void OnMouseOver()
     {
-        UIController.commandText = "Open";
-        UIController.uiActive = true;
+        if (PlayerCasting.distanceFromTarget < 5)
+        {
+            UIController.commandText = "Open";
+            UIController.uiActive = true;
+            canOpen = true;
+        }
+        else
+        {
+            UIController.uiActive = false;
+            canOpen = false;
+        }
     }
 
     void OnMouseExit()
     {
         UIController.commandText = "";
         UIController.uiActive = false;
+        canOpen = false;
+    }
+
+    IEnumerator OpeningDoor()
+    {
+        theCam.SetActive(true);
+        thePlayer.SetActive(false);
+        commandBox.SetActive(false);
+        interactCross.SetActive(false);
+        Cross.SetActive(false);
+        textDesc.SetActive(true);
+        lockedDoor.Play();
+        yield return new WaitForSeconds(3);
+        theCam.SetActive(false);
+        thePlayer.SetActive(true);
+        Cross.SetActive(true);
+        textDesc.SetActive(false);
     }
 }
